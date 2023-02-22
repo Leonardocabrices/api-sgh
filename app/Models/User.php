1075cba5b3a008bceeabda88019d565164748590
Leonardocabrices <leonardocabrices1@gmail.com>
2023-02-21 21:07:20 -0400

    agregar modificaciones en entidad roles y users

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,8 +19,11 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'last_name',
         'email',
         'password',
+        'role_id',
+        'status',
     ];
 
     /**
@@ -41,4 +44,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function role () {   
+        return $this->belongsTo(Role::class);
+    }
 }
